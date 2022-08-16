1a8db4be1e6c4098ba32387813e1748c3990ff47
mohamed <ma55r@outlook.com>
2022-08-16 13:31:49 +0300

    design show movie page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                '96': '24rem',
+            }
+        },
     },
     plugins: [],
 }
